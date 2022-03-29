780b71cf4f5ac87059d378ccf0ae6d7df01ee450
Naveenchand06 <naveenchand0606@gmail.com>
2022-03-29 23:28:01 +0530

    auth navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/login_screen.dart';
 import 'package:toast/toast.dart';
 
 void main() {
@@ -13,122 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  String _email = '';
-  String _password = '';
-
-  bool showPassword = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // backgroundColor: Colors.blueGrey.shade800,
-
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Login',
-                  style:
-                      TextStyle(fontSize: 30.0, fontWeight: FontWeight.w700)),
-              SizedBox(
-                height: 30.0,
-              ),
-              TextField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter Email',
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.teal),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.teal,
-                      width: 2.0,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.teal,
-                      width: 2.0,
-                    ),
-                  ),
-                ),
-                onChanged: (value) {
-                  _email = value;
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextField(
-                obscureText: showPassword,
-                decoration: InputDecoration(
-                  hintText: 'Enter password',
-                  labelText: 'Password',
-                  labelStyle: TextStyle(color: Colors.teal),
-                  suffixIcon: IconButton(
-                    icon: Icon(Icons.remove_red_eye_outlined),
-                    color: Colors.teal,
-                    onPressed: () {
-                      setState(() => showPassword = !showPassword);
-                    },
-                  ),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.teal,
-                      width: 2.0,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.teal,
-                      width: 2.0,
-                    ),
-                  ),
-                ),
-                onChanged: (value) {
-                  _password = value;
-                },
-              ),
-              SizedBox(
-                height: 40.0,
-              ),
-              OutlinedButton(
-                child: Text('Login'),
-                style: OutlinedButton.styleFrom(
-                  primary: Colors.white,
-                  backgroundColor: Colors.teal,
-                ),
-                onPressed: () {
-                  if (_email.contains('@')) {
-                    print(_email);
-                    print(_password);
-                  }
-                  Toast.show(
-                    'Successfully logged in',
-                    context,
-                    duration: 3,
-                  );
-                },
-              )
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
